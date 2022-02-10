387f7119a913132d4bd488189e3e4cc116acfd53
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-02-10 18:49:25 +0700

    create test ScheduledExecutor

---
+++ src/test/java/rifqimuhammadaziz/concurrency/ScheduledExecutorTest.java
@@ -0,0 +1,29 @@
+package rifqimuhammadaziz.concurrency;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ScheduledExecutorTest {
+
+    @Test
+    void delayedJob() throws InterruptedException {
+        var executor = Executors.newScheduledThreadPool(10);
+
+        var future = executor.schedule(() -> System.out.println("Hello ... "), 5, TimeUnit.SECONDS);
+
+        System.out.println(future.getDelay(TimeUnit.MILLISECONDS));
+        executor.awaitTermination(1, TimeUnit.DAYS);
+    }
+
+    @Test
+    void periodicJob() throws InterruptedException {
+        var executor = Executors.newScheduledThreadPool(10);
+
+        var future = executor.scheduleAtFixedRate(() -> System.out.println("Hello ... "), 2, 2, TimeUnit.SECONDS);
+
+        System.out.println(future.getDelay(TimeUnit.MILLISECONDS));
+        executor.awaitTermination(1, TimeUnit.DAYS);
+    }
+}
